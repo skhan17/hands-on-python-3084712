2033cea5b64cd798b70a5b811e3c398a5e93b30d
skhan17 <68302097+skhan17@users.noreply.github.com>
2023-02-02 23:14:08 +0000

    updated files with comments and code pt 2

--- Ex_Files/04_02_begin/main.py
+++ Ex_Files/04_02_begin/main.py
@@ -1,3 +1,12 @@
+from distutils.log import debug
 from flask import Flask
 
+# instantiate Flask object called app
 app = Flask(__name__)
+
+# define route
+@app.route("/")
+def hello():
+  return "Hello, World!"
+
+app.run(debug=True)
--- Ex_Files/04_04/app.py
+++ Ex_Files/04_04/app.py
@@ -28,8 +28,11 @@ def laureate_list():
     for laureate in laureates:
         surname = laureate["surname"].lower()
         # your code here
-        if search_string in surname:
+        flName = laureate["name"].lower()
+        if (search_string in surname) or (search_string in flName):
             results.append(laureate)
+        # elif search_string in flName:
+        #     results.append(laureate)
 
     return jsonify(results)
 
